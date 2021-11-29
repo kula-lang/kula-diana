d73542ada358cc311a26af43f20ef1041598d483
HanaYabuki <1847196775@qq.com>
2021-11-29 12:51:26 +0800

    nothing, just update

--- src/extension.js
+++ src/extension.js
@@ -1,11 +1,6 @@
 const vscode = require('vscode');
 
-
 exports.activate = function(context) {
-    
-    context.subscriptions.push(vscode.commands.registerCommand('extension.sayHello', function () {
-        vscode.window.showInformationMessage('Kula : わかってる...');
-    }));
 };
 
 
